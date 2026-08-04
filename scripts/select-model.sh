101306c802f4f2688bbfd9f1bf3d3323f054a45b
Nihat90 <nihat121090@googlemail.com>
2026-08-04 15:43:58 +0200

    Update resource group and workspace name in script

--- scripts/select-model.sh
+++ scripts/select-model.sh
@@ -2,5 +2,5 @@ az ml model create \
   --name superstore-loss-rf \
   --path azureml://jobs/DEIN_BESTER_CHILD_JOB/outputs/model_output/paths/model.joblib \
   --type custom_model \
-  --resource-group "DEINE_RESOURCE_GROUP" \
-  --workspace-name "DEIN_WORKSPACE"
+  --resource-group rg-ai300-l453a46dc08054eb79a \
+  --workspace-name mlw-ai300-l453a46dc08054eb79a
